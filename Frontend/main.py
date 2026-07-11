7304c9a41047bf0dfd3b5299b358840d55609c01
y4ric <ran23wert@gmail.com>
2026-07-11 22:09:09 +0300

    AESRDTGYUHJKL;

---
+++ Frontend/main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from streamlit import session_state
+
+st.title("""Регистрация""")
+
+if "login" not in session_state:
+    session_state["login"] = ""
+
+if "psw" not in session_state:
+    session_state["psw"] = ""
+
+st.subheader("Логин")
+login = st.text_input("Логин", value=session_state["login"], placeholder="Введите логин...")
+
+st.subheader("Пароль")
+psw = st.text_input("Пароль", value=session_state["psw"], placeholder="Введите пароль...")
+
+if st.button("Другая страница"):
+    st.switch_page("pages/another.py")
+
+st.text(f"Ваш пароль: {psw}")
+st.text(f"Ваше Имя: {login}")
+
+st.session_state["login"] = login
+st.session_state["psw"] = psw
---
+++ Frontend/pages/another.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.write(f'Ваш пароль (мы все еще его знаем!): {st.session_state["psw"]}')
